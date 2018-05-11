1edb14a1a2f901ab320fff994d352300f400c12b
John Moranski <johnnymoranski21@gmail.com>
2018-05-11 13:38:43 -0500

    Added to Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,11 +11,20 @@ class Navbar extends Component {
 
 render(){
 	return(
-		<nav class="navbar navbar-light bg-light">
-		  <a class="navbar-brand" href="#">
-		    <img src='./SMT_Logo.jpg' width="250" height="150" class="d-inline-block align-top" alt="" />
-		    Stamford Massage Therapy
-		  </a>
+		<nav className="navbar navbar-inverse navbar-fixed-top">
+			<div className="container-fluid">
+			<div className="navbar-header">
+			  <a className="navbar-brand">
+		    <img src='./SMT_Logo.jpg' width="250" height="150" className="d-inline-block align-top"  />
+		    Stamford Massage Therapy</a>
+		  <ul className="nav navbar-nav">
+		  <li className="active"><a href="#">Welcome</a></li>
+		  <li><a href="#">Testimonials</a></li>
+		  <li><a href="#">Meet The Staff</a></li>
+		  <li><a href="#">Contact Us</a></li>
+		  </ul>
+		  </div>
+		  </div>
 		</nav>
 		);
 	}
